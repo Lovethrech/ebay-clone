942b5f6a4d7d0bde4cae5b9aaa2d47e46e5b49a7
Lovethrech <rachealdolapo45@gmail.com>
2024-08-16 13:16:08 +0100

    working on Logo header...

--- src/components/SearchTab.vue
+++ src/components/SearchTab.vue
@@ -8,12 +8,21 @@
 }
 .main-ctn input{
     width:40vw;
-    height:30px;
+    height:40px;
 }
 .search-icon{
     display:flex;
     justify-content: center;
     place-items: center;
+    margin-right:-20px;
+}
+.main-ctn input{
+    background-color:transparent;
+    padding:0 20px;
+    border-radius:20px;
+}
+.desc{
+    margin:auto 0;
 }
 </style>
 
@@ -23,10 +32,10 @@
             <div class="search-icon">
                 <i class='bx bx-search-alt-2'></i>
             </div>
-            <input type="text" name="search">
+            <input type="text" name="search" placeholder="Search for anything">
             <div class="all-cat">
                 <div class="desc">
-                    <p>All <br> Categories</p>
+                    <p>All Categories</p>
                     <i class='bx bx-chevron-down'></i>
                 </div>
             </div>
